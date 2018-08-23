bb93146149a9b5b80c1e51d3a147c04a72b4cd4d
Reshleman17 <42658388+Reshleman17@users.noreply.github.com>
2018-08-23 17:46:37 -0500

    Hack

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ryan Eshleman
+ * Date: 2018/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Ryan Eshleman, Electrical Engineering!\n");
+
+  return 0;
+}
